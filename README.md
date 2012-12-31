25276aa4c8af17366820958f01aab07a014384ee
Jack Parker <parkej3@gmail.com>
2012-12-31 17:59:29 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-BAFA
-=====
+# BAFA - latest versions of Bootstrap, Animate.css, and Font-Awesome, packaged for Meteor
 
-Meteor Smart Package - Latest versions of Bootstrap, Animate.css, and Font-Awesome
+## How to install
+1. `npm install -g meteorite` (if not already installed)
+2. `mrt add bafa`
+3. Read documentation below and have fun!
+
+## Documentation
+* [Bootstrap 2.2.2](http://twitter.github.com/bootstrap/)
+* [Animate.css](http://daneden.me/animate/)
+* [Font-Awesome 2.0](http://fortawesome.github.com/Font-Awesome/)
---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+	summary: "BAFA - latest versions of Bootstrap, Animate.css, and Font-Awesome"
+});
+
+Package.on_use(function(api){
+	api.use('jquery', 'client');
+	api.add_files([
+		'lib/bootstrap-2.2.2/bootstrap.min.css',
+		'lib/bootstrap-2.2.2/bootstrap.min.js',
+		'lib/font-awesome-2.0/font/fontawesome-webfont.eot',
+		'lib/font-awesome-2.0/font/fontawesome-webfont.svg',
+		'lib/font-awesome-2.0/font/fontawesome-webfont.ttf',
+		'lib/font-awesome-2.0/font/fontawesome-webfont.woff',
+		'lib/font-awesome-2.0/font-awesome.css',
+		'lib/animate.css/animate.min.css'
+	], 'client');
+});
