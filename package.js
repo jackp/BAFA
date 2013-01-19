6ff2948f82cc43a055f41e041fb946746ae95ed6
jackp <parkej3@gmail.com>
2013-01-19 18:25:51 -0500

    removed animate.css

--- package.js
+++ package.js
@@ -7,11 +7,12 @@ Package.on_use(function(api){
 	api.add_files([
 		'lib/bootstrap-2.2.2/bootstrap.min.css',
 		'lib/bootstrap-2.2.2/bootstrap.min.js',
-		'lib/font-awesome-3.0/font/fontawesome-webfont.eot',
-		'lib/font-awesome-3.0/font/FontAwesome.otf',
-		'lib/font-awesome-3.0/font/fontawesome-webfont.ttf',
-		'lib/font-awesome-3.0/font/fontawesome-webfont.woff',
-		'lib/font-awesome-3.0/font-awesome.css',
+		'lib/font-awesome-3.0.2/font/fontawesome-webfont.eot',
+		'lib/font-awesome-3.0.2/font/FontAwesome.otf',
+		'lib/font-awesome-3.0.2/font/fontawesome-webfont.ttf',
+		'lib/font-awesome-3.0.2/font/fontawesome-webfont.woff',
+		'lib/font-awesome-3.0.2/font/fontawesome-webfont.svg',
+		'lib/font-awesome-3.0.2/css/font-awesome.css',
 		'lib/animate.css/animate.min.css'
 	], 'client');
 });
